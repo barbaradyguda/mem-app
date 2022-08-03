31a28288b5c9583fd56da2d801df635b95e8d343
barbaradyguda <41574313+barbaradyguda@users.noreply.github.com>
2022-08-03 12:24:44 +0200

    another idea :)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 import Mem from "./components/Mem"
-import Memes from "./components/Memes"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [memUrls, setMemUrls] = useState([])
+  const [currentMemUrlLeft, setCurrentMemUrlLeft] = useState("")
+  const [currentMemUrlRight, setCurrentMemUrlRight] = useState("")
+
+  useEffect(() => {
+    getMemData()
+  }, []);
+
+  const getMemData = async () => {
+    await axios
+      .get(`https://api.imgflip.com/get_memes`)
+      .then(async (response) => {
+        const memData = response.data;
+        getUrls(memData.data.memes)
+      })
+      .catch((err) => console.debug(err));
+  };
+
+  const getUrls = (memes) => {
+    const pluck = (arr, key) => arr.map(i => i[key]);
+    let urls = pluck(memes, 'url')
+    setMemUrls(urls);
+    setCurrentMemUrlLeft(urls[0])
+    setCurrentMemUrlRight(urls[0])
+  }
+
+
   return (
-    <div className="App">
- {/* <Mem/> */}
- <Memes/>
+    <div style={{ display: "flex" }}>
+      <Mem memUrls={memUrls} currentMemUrl={currentMemUrlLeft} setCurrentMemUrl={setCurrentMemUrlLeft} />
+      <Mem memUrls={memUrls} currentMemUrl={currentMemUrlRight} setCurrentMemUrl={setCurrentMemUrlRight} />
     </div>
   );
 }
--- src/components/Mem.js
+++ src/components/Mem.js
@@ -1,36 +1,13 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
-const Mem = () => {
-    const [memUrls, setMemUrls] = useState([])
+const Mem = ({ memUrls,currentMemUrl, setCurrentMemUrl }) => {
+
     const [currentIndex, setCurrentIndex] = useState(0)
-    const [currentMemUrl, setCurrentMemUrl] = useState("")
-
-    useEffect(() => {
-        getMemData()
-    }, []);
-
 
     useEffect(() => {
         setCurrentUrl()
     }, [currentIndex]);
 
-    const getMemData = async () => {
-        await axios
-            .get(`https://api.imgflip.com/get_memes`)
-            .then(async (response) => {
-                const memData = response.data;
-                getUrls(memData.data.memes)
-            })
-            .catch((err) => console.debug(err));
-    };
-
-    const getUrls = (memes) => {
-        const pluck = (arr, key) => arr.map(i => i[key]);
-        let urls = pluck(memes, 'url')
-        setMemUrls(urls);
-        setCurrentMemUrl(urls[0])
-    }
 
     const setCurrentUrl = () => {
         setCurrentMemUrl(memUrls[currentIndex])
@@ -38,7 +15,7 @@ const Mem = () => {
 
     return (
 
-        <div style={{ display: "flex", justifyContent: "center", backgroundColor: "#eaeaea", alignItems: "center", height: "100vh" }}>
+        <div style={{ width: "50vw", display: "flex", justifyContent: "center", backgroundColor: "#eaeaea", alignItems: "center", height: "100vh" }}>
 
             {currentIndex !== 0 && <button onClick={() => setCurrentIndex(currentIndex - 1)}>Prev</button>}
             <img src={currentMemUrl} width="50%" />
